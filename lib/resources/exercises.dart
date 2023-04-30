d9ffdf4237c7afc54c2c5039c52fa0db91fd1c44
jferrom780 <jferrom780@g.educaand.es>
2023-04-30 20:21:27 +0200

    javier: Add MeasuresType

--- lib/resources/exercises.dart
+++ lib/resources/exercises.dart
@@ -11,29 +11,41 @@ enum ExercisesType {
   const ExercisesType(this.name);
 }
 
+enum MeasuresType {
+  reps("sets_reps"),
+  meters("sets_meters"),
+  seconds("sets_seconds"),
+  lbs("sets_reps_lb"),
+  none("");
+  final String type;
+
+  const MeasuresType(this.type);
+}
+
 enum Exercises {
-  burpees(ExercisesType.gymnastics, "Burpees", "https://youtu.be/hAeyJGLnrxQ"),
-  push_up(ExercisesType.gymnastics, "Push up", "https://youtu.be/M1IfJmVjKW0"),
-  squads(ExercisesType.gymnastics, "Squads", "https://youtu.be/xDdSZmWNYQI"),
-  pull_up(ExercisesType.gymnastics, "Pulls Up", "https://youtu.be/ifOBltCCRZw"),
-  plank(ExercisesType.gymnastics, "Plank", "https://youtu.be/CO7MktWaoD8"),
-  bench_press(ExercisesType.strength, "Bench Press", "https://youtu.be/FgVDxCxXkmc"),
-  wall_ball(ExercisesType.strength, "Wall Ball", "https://youtu.be/UUo2ONp4iGc"),
-  front_squad(ExercisesType.strength, "Front Squad", "https://youtu.be/UoikaYjLRNk"),
-  swing(ExercisesType.strength, "Bell Swing", "https://youtu.be/0dvov7IHvL0"),
-  deadlift(ExercisesType.strength, "Deadlift", "https://youtu.be/qCGtwPhfGf4"),
-  run(ExercisesType.strengthening, "Run", "https://youtu.be/gF0rrpMH-Jo"),
-  stairs(ExercisesType.strengthening, "Stairs", "https://youtu.be/cjjiSpM8OEs"),
-  sprint(ExercisesType.strengthening, "Sprint", "https://youtu.be/QOnX8cVbbdE"),
-  single_unders(ExercisesType.strengthening, "Single Under", "https://youtu.be/ZV10GlZk4To"),
-  jump_squat(ExercisesType.strengthening, "Jump Squat", "https://youtu.be/J6Y520KkwOA"),
-  none(ExercisesType.none, "", "");
+  burpees(ExercisesType.gymnastics, "Burpees", "https://youtu.be/hAeyJGLnrxQ", MeasuresType.reps),
+  push_up(ExercisesType.gymnastics, "Push up", "https://youtu.be/M1IfJmVjKW0", MeasuresType.reps),
+  squads(ExercisesType.gymnastics, "Squads", "https://youtu.be/xDdSZmWNYQI", MeasuresType.reps),
+  pull_up(ExercisesType.gymnastics, "Pulls Up", "https://youtu.be/ifOBltCCRZw", MeasuresType.reps),
+  plank(ExercisesType.gymnastics, "Plank", "https://youtu.be/CO7MktWaoD8", MeasuresType.reps),
+  bench_press(ExercisesType.strength, "Bench Press", "https://youtu.be/FgVDxCxXkmc", MeasuresType.lbs),
+  wall_ball(ExercisesType.strength, "Wall Ball", "https://youtu.be/UUo2ONp4iGc", MeasuresType.lbs),
+  front_squad(ExercisesType.strength, "Front Squad", "https://youtu.be/UoikaYjLRNk", MeasuresType.lbs),
+  swing(ExercisesType.strength, "Bell Swing", "https://youtu.be/0dvov7IHvL0", MeasuresType.lbs),
+  deadlift(ExercisesType.strength, "Deadlift", "https://youtu.be/qCGtwPhfGf4", MeasuresType.lbs),
+  run(ExercisesType.strengthening, "Run", "https://youtu.be/gF0rrpMH-Jo", MeasuresType.meters),
+  stairs(ExercisesType.strengthening, "Stairs", "https://youtu.be/cjjiSpM8OEs", MeasuresType.meters),
+  sprint(ExercisesType.strengthening, "Sprint", "https://youtu.be/QOnX8cVbbdE", MeasuresType.meters),
+  single_unders(ExercisesType.strengthening, "Single Under", "https://youtu.be/ZV10GlZk4To", MeasuresType.reps),
+  jump_squat(ExercisesType.strengthening, "Jump Squat", "https://youtu.be/J6Y520KkwOA", MeasuresType.reps),
+  none(ExercisesType.none, "", "", MeasuresType.none);
 
   final ExercisesType type;
   final String name;
   final String url;
+  final MeasuresType measuresType;
 
-  const Exercises(this.type, this.name, this.url);
+  const Exercises(this.type, this.name, this.url, this.measuresType);
 }
 
 Map<String, Exercises> strToExercise = {
